b4ba4df5b11b96ee797cd9bc8d889befb234b90b
JP <juanpablo@rindegastos.com>
2022-06-13 14:04:48 +0200

    Tipos Number, String y Boolean

--- dist/main.js
+++ dist/main.js
@@ -1,2 +1,32 @@
 "use strict";
-console.log('Hola TypeScript con Platzi');
+console.log("Hola TypeScript con Platzi");
+// Number
+// Explícito
+var phone;
+phone = 1;
+phone = 54234567;
+// phone = 'hola'; // Error
+// Inferido
+var phoneNumber = 54234567;
+phoneNumber = 123;
+// phoneNumber = true // Error
+var hex = 0xf00d;
+var binary = 10;
+var octal = 484;
+// Boolean
+var isPro = true;
+isPro = false;
+// isPro = 1; // Error
+// Inferido
+var isUserPro = false;
+isUserPro = true;
+// isUserPro = 1; // Error
+// String
+var username = "jpmoya";
+username = "Juan";
+// username = true; // Error
+// Template String
+// Uso del caracter especial `
+var userInfo;
+userInfo = "\n    User Info:\n    username: ".concat(username, "\n    firstName: ").concat(username + " Aviles", "\n    phone: ").concat(phone, "\n    isPro: ").concat(isUserPro, "\n");
+console.log("userInfo", userInfo);
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,45 @@
-console.log('Hola TypeScript con Platzi');
+console.log("Hola TypeScript con Platzi");
+
+// Number
+// Explícito
+let phone: number;
+phone = 1;
+phone = 54234567;
+// phone = 'hola'; // Error
+
+// Inferido
+let phoneNumber = 54234567;
+phoneNumber = 123;
+
+// phoneNumber = true // Error
+
+let hex: number = 0xf00d;
+let binary: number = 0b1010;
+let octal: number = 0o744;
+
+// Boolean
+let isPro: boolean = true;
+isPro = false;
+// isPro = 1; // Error
+
+// Inferido
+let isUserPro = false;
+isUserPro = true;
+// isUserPro = 1; // Error
+
+// String
+let username: string = "jpmoya";
+username = "Juan";
+// username = true; // Error
+
+// Template String
+// Uso del caracter especial `
+let userInfo: string;
+userInfo = `
+    User Info:
+    username: ${username}
+    firstName: ${username + " Aviles"}
+    phone: ${phone}
+    isPro: ${isUserPro}
+`;
+console.log("userInfo", userInfo);
